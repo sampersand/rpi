004aa0d773690216b6da217be820191c62aac9ee
SamW <mail@sampersand.me>
2026-04-13 13:59:30 -0700

    added the h tool, and hello worlds

---
+++ webserver/serve/hello-world/trivial.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This is a hello world with a simple `cat` program, and only minimal
+# outside batteries
+
+# Make sure it's a GET request; `$METHOD` is provided by the harness
+if [ $METHOD != GET ]; then
+	status 405 true
+	exit
+fi
+
+# post request params are set as env vars; if `$place` wasn't provided, default
+# to `world`.
+: ${place:=world}
+
+header Content-Type text/html
+
+cat <<EOS
+<!DOCTYPE html>
+<html lang=en>
+<head>
+	<title>Hello $place!</title>
+</head>
+<body>
+	<h1>Hello, $place!</h1>
+	<p>The time on the server is currently `date`.</p>
+</body>
+</html>
+EOS
